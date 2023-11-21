940c477cfb2d0695db0b908b0f5eb4ba528ce263
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2023-11-21 11:24:58 +0000

    Add devcontainer.json and Playground project files

---
+++ demos/dotnet/Playground/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CopilotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius of the circle:");
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double radius))
+            {
+                double area = Math.PI * radius * radius;
+                Console.WriteLine($"The area of the circle is: {area}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a numeric value.");
+            }
+        }
+    }
+}
