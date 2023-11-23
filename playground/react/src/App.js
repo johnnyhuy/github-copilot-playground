6aea0151e5c3e3f87579afa2ce100c17abc34447
Johnny Huynh <27847622+johnnyhuy@users.noreply.github.com>
2023-11-24 08:52:31 +1100

    Add date.py, donut.py, and fibonacci.py files, and update App.js in React Playground

--- playground/react/src/App.js
+++ playground/react/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
-import './App.css';
 
 
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>React Playground</h1>
+      </header>
       {/* create a hero page with the words Hello world and welcome to GitHub Copilot! */}
     </div>
   );
